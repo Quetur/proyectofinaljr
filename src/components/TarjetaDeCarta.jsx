5be2838e7da6ff4d5a3d3676ae7803b65089b1cc
quetur <jrosavila@gmail.com>
2025-06-27 16:49:27 -0300

    suma del carrito

--- src/components/TarjetaDeCarta.jsx
+++ src/components/TarjetaDeCarta.jsx
@@ -5,17 +5,21 @@ import miLogo from './micard.png';
 const TarjetaDeCarta = ({ carta, agregarAlCarrito }) => {
   return (
     //muestro las cards
+   
     <Container className="mt-4">
 
       <Row>
             <Card className="m-2" >
+               {console.log("tarjeta ", carta)}
               <Card.Img src={carta.imageUrl || miLogo}/>
                 <Card.Body>
                   <Card.Title>Nombre: {carta.name}</Card.Title>
                     <Card.Text>
                         <strong>Tipo: {carta.type || 'N/A'}</strong>
                     </Card.Text>
-                   
+                    <Card.Text>
+                        <strong>${carta.cmc}</strong>
+                    </Card.Text>
 
                       <Button variant="primary" onClick={() => agregarAlCarrito(carta)}> Agregar al carrito   </Button>
                 </Card.Body>
