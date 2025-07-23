163f4a8acba7a650403b2fb5f453811daaed5eec
quetur <jrosavila@gmail.com>
2025-07-23 17:32:26 -0300

    para entregar

--- src/paginas/ListarTodasLasCartas.jsx
+++ src/paginas/ListarTodasLasCartas.jsx
@@ -1,13 +1,11 @@
 import ListaDeCartas from '../components/ListaDeCartas'
-import Listado from '../components/Listado'
+
 
 const ListarTodasLasCartas = () => {
   return (
     <div className="container">
       <h1>Todas las cartas</h1>
-        {/*<ListaDeCartas />*/}
-                
-        <Listado />
+        <ListaDeCartas />        
     </div>
   )
 }
